5e83b0bc56c04afe5e40702a424dd942062388ee
Rcaccioppoli <Caccioppolirocco@gmail.com>
2016-11-29 21:23:23 +0100

    Da completare: Ultimi 4 task

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -2,8 +2,78 @@ package it.uniba.tdd;
 
 public class StringCalculator {
 	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+		int result=0;
+		String[] numbers;
+		
+		if (numbersStr.length()==0)
+			result=0;
+		else{
+			numbers = parser(numbersStr);
+			try {
+				result = total(numbers);
+			} catch (StringCalculatorException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	private String[] parser(String numbersStr) {
+		String parsedString="";
+		String delimiter = createDelimiter(numbersStr);
+		
+		for (int i=0; i<numbersStr.length(); i++) {
+			if (Character.isDigit(numbersStr.charAt(i)) || numbersStr.charAt(i)=='-')
+				parsedString+=numbersStr.charAt(i);
+			else
+				parsedString+=delimiter;
+		}
+		
+		return removeEmpty(parsedString.split(delimiter));
+	}
+
+	private String[] removeEmpty(String[] split) {
+		int j=0;
+		
+		for (int i=0; i<split.length; i++)
+			if (!split[i].isEmpty())
+			{
+				split[j]=split[i];
+				j++;
+			}
+		
+		String[] result = new String[j];
+		
+		System.arraycopy(split, 0, result, 0, j);
+		
+		return result;
+	}
+
+	private String createDelimiter(String numbersStr) {
+		String delimiter = "";
+		
+		if(numbersStr.split("\n").length > 1)
+			if (numbersStr.split("\n")[0].split("//").length > 1)
+				delimiter=numbersStr.split("\n")[0].split("//")[1];
+		
+		if(delimiter.isEmpty())
+			delimiter=",";
+		
+		return delimiter;
+	}
+
+	private int total(String[] numbers) throws StringCalculatorException {
+		int result = 0;
+		
+		for (int i=0; i<numbers.length; i++) {
+			if (Integer.parseInt(numbers[i]) >= 0) {
+				result += Integer.parseInt(numbers[i]);
+			}
+			else
+				throw new StringCalculatorException();	
+		}
+		
+		return result;
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -2,13 +2,71 @@ package it.uniba.tdd.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import it.uniba.tdd.StringCalculator;
 
 public class StringCalculatorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	StringCalculator calculator;
+	String emptyString;
+	String oneDigits;
+	String twoDigits;
+	String twoNumbers;
+	String moreNumbers;
+	String noComma;
+	String incorrectNoComma;
+	String differentDelimiter;
+	String negativeNumbers;
+	
+	@Before
+	public void setup() {
+		emptyString = "";
+		oneDigits = "1";
+		twoDigits = "12";
+		twoNumbers = "12,3";
+		moreNumbers = "12,34,56,78,90";
+		noComma = "â€œ1\n2,3â€";
+		incorrectNoComma = "â€œ1,\nâ€";
+		differentDelimiter = "//;\n1;2";
+		negativeNumbers = "//=\n-1=-2";
+		calculator = new StringCalculator();
 	}
-
+	
+	@Test
+	public void emptyStringTest() {
+		assertEquals(0, calculator.add(emptyString));
+	}
+	
+	@Test
+	public void oneNumberTest() {
+		assertEquals(1, calculator.add(oneDigits));
+		assertEquals(12, calculator.add(twoDigits));
+	}
+	
+	@Test
+	public void twoNumberTest() {
+		assertEquals(15, calculator.add(twoNumbers));
+	}
+	
+	@Test
+	public void moreNumberTest() {
+		assertEquals(270, calculator.add(moreNumbers));
+	}
+	
+	@Test
+	public void noCommaTest() {
+		assertEquals(6, calculator.add(noComma));
+		assertEquals(1, calculator.add(incorrectNoComma)); //da implementare
+	}	
+	
+	@Test
+	public void differentDelimiterTest() {
+		assertEquals(3, calculator.add(differentDelimiter));
+	}	
+	
+	@Test
+	public void negativeNumbersTest() {
+		//assertEquals(-3, calculator.add(negativeNumbers));
+	}	
 }
